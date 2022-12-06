e7383b8641f6995e63dc73001e3034416d3908b0
sathwik2009 <93324037+sathwik2009@users.noreply.github.com>
2022-12-06 18:51:11 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,7 +1,48 @@
+noseX=0;
+noseY=0;
+difference=0;
+rightWristX=0;
+leftWristX=0;
+
 function setup(){
     canvas=createCanvas(400, 400);
     canvas.position(560, 150);
     
     video=createCapture(VIDEO);
     video.size(550, 500)
+
+    poseNet=ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+function modelLoaded() {
+    console.log('PoseNet is Initialized!');
+}
+
+function gotPoses(results)
+{
+    if(results.length > 0)
+    {
+        console.log(results);
+        noseX=results[0].pose.nose.x;
+        noseY=results[0].pose.nose.y;
+        console.log("noseX="+noseX+"noseY="+noseY);
+
+        leftWristX=results[0].pose.leftWrist.x;
+        rightWristX=results[0].pose.rightWrist.x;
+        difference=floor(leftWristX-rightWristX);
+
+        console.log("leftWristX="+leftWristX+"rightWristX="+rightWristX);
+    }
+}
+
+function draw() {
+    background('#969A97');
+
+    document.getElementById("square_side").innerHTML=" font size= "+difference+"px";
+    fill('#03e2ff');
+    stroke('#ff0303');
+    textSize(difference-100);
+    text("SAI", noseX, noseY);
+
 }
